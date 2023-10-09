fc15a9f1606eb57b7e045b48a4e9fb61f539bf6a
maquico <1104666@est.intec.edu.do>
2023-10-08 21:54:06 -0400

    schema update

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 DROP DATABASE VideosApp_db;
 CREATE DATABASE VideosApp_db;
+
 USE VideosApp_db;
 
 CREATE TABLE `VISIBILIDAD` (
@@ -87,12 +88,13 @@ CREATE TABLE `VIDEOS_TAGS` (
   FOREIGN KEY (`video_id`) REFERENCES `VIDEOS`(`video_id`)
 );
 
+DROP TABLE IF EXISTS `SUSCRIPCIONES`;
+
 CREATE TABLE `SUSCRIPCIONES` (
-  `suscripcion_id` INT NOT NULL AUTO_INCREMENT,
   `suscriptor_id` INT NOT NULL,
   `canal_id` INT NOT NULL,
   `fecha_reg` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-  PRIMARY KEY (`suscripcion_id`),
+  PRIMARY KEY (`suscriptor_id`, `canal_id`),
   FOREIGN KEY (`suscriptor_id`) REFERENCES `USUARIOS`(`user_id`),
   FOREIGN KEY (`canal_id`) REFERENCES `USUARIOS`(`user_id`)
 );
