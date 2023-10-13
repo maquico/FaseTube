5189bd5c0acfa405c040db1b6e52d0e5174a6b69
Francisco <drmalito.weid@gmail.com>
2023-10-13 12:47:06 -0400

    Busqueda implementada by Sebastián

--- client/src/components/LargeCardVideo.jsx
+++ client/src/components/LargeCardVideo.jsx
@@ -1,4 +1,7 @@
+import { Link } from "react-router-dom";
+
 export default function LargeCardVideo({
+  video_id,
   miniatura,
   titulo,
   vistas,
@@ -8,12 +11,17 @@ export default function LargeCardVideo({
 }) {
   return (
     <div className="flex my-4">
-      <div className="flex w-[285px] h-[160px] rounded-lg bg-white mx-4 justify-end items-start">
-        {/* <img className="w-[285px] h-[160px] object-cover focus:outline-none" /> */}
-        <span className="absolute bg-black opacity-90 m-2 p-1 rounded-md text-white text-sm font-serif z-30">
-          20:60
-        </span>
-      </div>
+      <Link to={"/ver/" + video_id}>
+        <div className="flex w-[285px] h-[160px] rounded-lg bg-white mx-4 justify-end items-start">
+          <img
+            className="w-[285px] h-[160px] object-cover focus:outline-none"
+            src={`https://storage.googleapis.com/fase-tube-server-bucket/${miniatura}`}
+          />
+          <span className="absolute bg-black opacity-90 m-2 p-1 rounded-md text-white text-sm font-serif z-30">
+            20:60
+          </span>
+        </div>
+      </Link>
       <div>
         <h2 className="text-2xl block">{titulo}</h2>
         <p>{vistas} vistas</p>
--- client/src/pages/BusquedaPage.jsx
+++ client/src/pages/BusquedaPage.jsx
@@ -6,15 +6,20 @@ import axios from "axios";
 
 export default function BusquedaPage() {
   const { busqueda } = useParams();
-
   const [resultados, setResultados] = useState([]);
 
-  // useEffect(() => {
-  //   axios
-  //     .get(`un string que no conozco ${busqueda}`)
-  //     .then((response) => setResultados(response.data))
-  //     .catch((error) => console.error("Error fetching videos:", error));
-  // }, []);
+  useEffect(() => {
+    axios
+      .get(
+        `https://fase-tube-server-c537f172c3b7.herokuapp.com/api/busqueda/?text=${busqueda}`
+      )
+      .then((response) => {
+        setResultados(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching videos:", error);
+      });
+  }, [busqueda]);
 
   return (
     <div className="flex">
@@ -25,6 +30,8 @@ export default function BusquedaPage() {
           <div className="w-[70vw] my-4">
             {resultados.map((video) => {
               <LargeCardVideo
+                video_id={video.video_id}
+                miniatura={video.miniatura_ruta}
                 titulo={video.titulo}
                 vistas={video.vistas}
                 tiempoSubido={video.tiempoSubido}
