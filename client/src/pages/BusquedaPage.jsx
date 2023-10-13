04d451aeb264e6747c0f827e83d0f6f2aa191962
sebas <sebastian.mercado1415@gmail.com>
2023-10-13 12:38:15 -0400

    busqeudasImplementadas

--- client/src/pages/BusquedaPage.jsx
+++ client/src/pages/BusquedaPage.jsx
@@ -1,9 +1,22 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams} from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function BusquedaPage() {
   const { busqueda } = useParams();
+  const [resultados, setResultados] = useState([])
 
+  useEffect(() => {
+    axios
+      .get(`https://fase-tube-server-c537f172c3b7.herokuapp.com/api/busqueda/?text=${busqueda}`)
+      .then((response) => {
+        setResultados(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching videos:', error);
+      });
+  }, [busqueda]);
   return (
     <div className="flex">
       <Sidebar />
@@ -11,41 +24,17 @@ export default function BusquedaPage() {
         <div className="font-serif text-white mx-2">
           <h1 className="text-2xl">Resultados de la busqueda: {busqueda}</h1>
           <div className="w-[70vw] my-4">
+          {resultados.map((info) => (
             <LargeCardVideo
-              titulo="Titulo 1"
-              vistas={123}
-              tiempoSubido="Hace 2 días"
-              canal="TheBestChannel"
-              descripcion="Hola 1"
+            video_id = {info.video_id}
+            miniatura = {info.miniatura_ruta}
+            titulo={info.titulo}
+            vistas={info.vistas}
+            tiempoSubido={info.tiempoSubido}
+            canal={info.canal}
+            descripcion={info.descripcion}
             />
-            <LargeCardVideo
-              titulo="Titulo 2"
-              vistas={123}
-              tiempoSubido="Hace 2 días"
-              canal="TheBestChannel"
-              descripcion="Hola 2"
-            />
-            <LargeCardVideo
-              titulo="Titulo 3"
-              vistas={123}
-              tiempoSubido="Hace 2 días"
-              canal="TheBestChannel"
-              descripcion="Hola 3"
-            />
-            <LargeCardVideo
-              titulo="Titulo 4"
-              vistas={123}
-              tiempoSubido="Hace 2 días"
-              canal="TheBestChannel"
-              descripcion="Hola 4"
-            />
-            <LargeCardVideo
-              titulo="Titulo 5"
-              vistas={123}
-              tiempoSubido="Hace 2 días"
-              canal="TheBestChannel"
-              descripcion="Hola 5"
-            />
+          ))}
           </div>
         </div>
       </div>
@@ -54,6 +43,7 @@ export default function BusquedaPage() {
 }
 
 function LargeCardVideo({
+  video_id,
   miniatura,
   titulo,
   vistas,
@@ -63,12 +53,14 @@ function LargeCardVideo({
 }) {
   return (
     <div className="flex my-4">
+      <Link to = {"/ver/"+ video_id}>
       <div className="flex w-[285px] h-[160px] rounded-lg bg-white mx-4 justify-end items-start">
-        {/* <img className="w-[285px] h-[160px] object-cover focus:outline-none" /> */}
+        <img className="w-[285px] h-[160px] object-cover focus:outline-none" src= {`https://storage.googleapis.com/fase-tube-server-bucket/${miniatura}`}/>
         <span className="absolute bg-black opacity-90 m-2 p-1 rounded-md text-white text-sm font-serif z-30">
           20:60
         </span>
       </div>
+      </Link>
       <div>
         <h2 className="text-2xl block">{titulo}</h2>
         <p>{vistas} vistas</p>
