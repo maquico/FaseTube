cb197d799cf7b01909cbbdfefa8a471045994cc1
maquico <1104666@est.intec.edu.do>
2023-10-13 04:32:57 -0400

    comments con info del user

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -6,11 +6,11 @@ const Comments = ({ comments }) => {
       {comments.map((comment) => (
         <div key={comment.comentario_id} className="flex items-center space-x-4 p-4 bg-gray-800 rounded-lg mb-4">
           <div>
-           
+            <img src={comment.USUARIOS.foto_ruta} alt="User Avatar" className="w-10 h-10 rounded-full" />
           </div>
           <div>
             <p className="text-white">{comment.contenido}</p>
-
+            <p className="text-gray-400">{comment.USUARIOS.username}</p>
           </div>
         </div>
       ))}
